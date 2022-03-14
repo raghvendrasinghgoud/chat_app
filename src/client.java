93dabed0454c1b461398a4dabbe9b1356551310f
raghvendrasinghgoud <raghavendrasinghgoud@gmail.com>
2022-03-14 23:29:29 +0530

    Exiting Exception fixed and Development for chat app ver 1.0 is over

--- src/client.java
+++ src/client.java
@@ -17,7 +17,7 @@ class client extends chat {
       dos = new DataOutputStream(socket.getOutputStream());
       dis = new DataInputStream(socket.getInputStream());
      // super.setName("Client");
-      this.stratThreads();
+     this.stratThreads();
     } catch (Exception e) {
       System.out.println("server not strate\n" + e.getMessage());
     }
@@ -32,44 +32,44 @@ class client extends chat {
 
   public void stratThreads() {
     Runnable thread1 = () -> {
+      try {
       while (true) {
-        try {
           if (this.status.equals("exit")) {
             dos.writeUTF("bye");
             dos.flush();
             break;
           }
           super.sendMessage(dos);
-        } catch (Exception e) {
-          e.printStackTrace();
         }
+      } catch (Exception e) {
+        e.printStackTrace();
       }
-    };
+      };
     t1 = new Thread(thread1);
 
 
     Runnable thread2 = () -> {
+      try {
       while (true) {
-        try {
           super.recieveMessage(dis);
           if (getmsg().equals("bye")) {
             this.status = "exit";
+            dos.writeUTF("bye");
+            t1.interrupt();
+            this.closeCon();
+            System.exit(0);
             break;
           }
-        } catch (Exception e) {
-          System.out.println("server get offline");
-          e.printStackTrace();
         }
+      } catch (Exception e) {
+        System.out.println("server get offline");
       }
     };
     t2 = new Thread(thread2);
-    try{
-    t1.join();
-        t2.join();
-        this.closeCon();
-    }catch(Exception e){ e.printStackTrace();}
-  }
 
+    t1.start();
+        t2.start();
+  }
   public static void main(String args[]) throws Exception {
     new client("localhost", 6666);
 
--- src/server.java
+++ src/server.java
@@ -20,57 +20,57 @@ class server extends chat {
             this.dis = new DataInputStream(this.socket.getInputStream());
            // super.setName("Server");
             this.stratThreads();
+            
         } catch (Exception e) {
             System.out.println("server not strated" + e.getMessage());
         }
     }
 
     @Override
-    public void closeCon() throws Exception {
+    public void closeCon(){
+        try{
         dos.close();
         dis.close();
         socket.close();
         ss.close();
+        }catch(Exception e){ System.out.println("Chatting off successfully");}
     }
 
     public void stratThreads() {
         Runnable thread1 = () -> {
-          while (true) {
             try {
-            
+            while (true) {            
                 if(this.status.equals("exit")){
-                    this.closeCon();
                     break;
                 }
                 super.sendMessage(dos);
-            } catch (Exception e) {
-              e.printStackTrace();
             }
-          }
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
         };
         t1 = new Thread(thread1);
     
         Runnable thread2 = () -> {
-          while (true) {
             try {
+          while (true) {
                 
                 super.recieveMessage(dis);
               if(getmsg().equals("bye")){
                   this.status="exit";
+                  t1.interrupt();
+                  this.closeCon();
+                  System.exit(0);
                   break;
-              }
-            } catch (Exception e) {
-              System.out.println("server get offline");
-              e.printStackTrace();
+                }
             }
-          }
+        } catch (Exception e) {
+          System.out.println("server get offline");
+        }
         };
         t2 = new Thread(thread2);
-        try{
-            t1.join();
-                t2.join();
-                this.closeCon();
-            }catch(Exception e){ e.printStackTrace();}
+            t1.start();
+                t2.start();
       }
 
     public static void main(String args[]) throws Exception {
